10578b785e83274a3aad2d1bfa44851d74555581
Tami Takada <tamitakada@gmail.com>
2025-06-23 11:06:58 -0400

    comment gurobi

--- core/simulation.py
+++ core/simulation.py
@@ -12,8 +12,8 @@ from core.external_client import *
 from core.events import *
 import pandas as pd
 
-import gurobipy as gp
-from gurobipy import GRB
+# import gurobipy as gp
+# from gurobipy import GRB
 
 
 class Simulation(object):
@@ -61,82 +61,92 @@ class Simulation(object):
         configs = [6, 12, 24]
         nodes = [i for i in range(TOTAL_NUM_OF_NODES)]
 
+        # ppl1
         throughput = {
             "0,2": {6: 200, 12: 240, 24: 270},
             "1": {24: 45},
             "3": {6: 55, 12: 55, 24: 70},
         }
+
+        # ppl2
+        throughput = {
+            '0': {12:71, 24: 125},
+            '1': {6: 5333, 12: 6083 ,24: 7555},
+            '2': {6: 26, 12: 45, 24: 92},
+            '3': {12: 3.9, 24: 4.82}
+        }
+
         valid_layouts = [[24], [12, 12], [12, 6, 6], [6, 6, 6, 6]]
 
-        def solve_leximin(locked_lower_bounds):
-            m = gp.Model("Leximin_Level")
-            x, y = {}, {}
-            T_m = {}
-            Z = m.addVar(lb=0, vtype=GRB.CONTINUOUS, name="Z")
+        # def solve_leximin(locked_lower_bounds):
+        #     m = gp.Model("Leximin_Level")
+        #     x, y = {}, {}
+        #     T_m = {}
+        #     Z = m.addVar(lb=0, vtype=GRB.CONTINUOUS, name="Z")
 
-            for model in models:
-                for node in nodes:
-                    for c in configs:
-                        if c in throughput[model]:
-                            x[model, node, c] = m.addVar(vtype=GRB.INTEGER, name=f"x_{model}_{node}_{c}")
+        #     for model in models:
+        #         for node in nodes:
+        #             for c in configs:
+        #                 if c in throughput[model]:
+        #                     x[model, node, c] = m.addVar(vtype=GRB.INTEGER, name=f"x_{model}_{node}_{c}")
 
-            for node in nodes:
-                for lid, layout in enumerate(valid_layouts):
-                    y[node, lid] = m.addVar(vtype=GRB.BINARY, name=f"y_{node}_{lid}")
+        #     for node in nodes:
+        #         for lid, layout in enumerate(valid_layouts):
+        #             y[node, lid] = m.addVar(vtype=GRB.BINARY, name=f"y_{node}_{lid}")
 
-            for model in models:
-                T_m[model] = m.addVar(lb=0, vtype=GRB.CONTINUOUS, name=f"T_{model}")
-                m.addConstr(
-                    T_m[model] == gp.quicksum(
-                        x[model, node, c] * throughput[model][c]
-                        for node in nodes for c in configs if (model, node, c) in x
-                    )
-                )
+        #     for model in models:
+        #         T_m[model] = m.addVar(lb=0, vtype=GRB.CONTINUOUS, name=f"T_{model}")
+        #         m.addConstr(
+        #             T_m[model] == gp.quicksum(
+        #                 x[model, node, c] * throughput[model][c]
+        #                 for node in nodes for c in configs if (model, node, c) in x
+        #             )
+        #         )
 
-                if model in locked_lower_bounds:
-                    m.addConstr(T_m[model] >= locked_lower_bounds[model])
-                else:
-                    m.addConstr(Z <= T_m[model])
+        #         if model in locked_lower_bounds:
+        #             m.addConstr(T_m[model] >= locked_lower_bounds[model])
+        #         else:
+        #             m.addConstr(Z <= T_m[model])
 
-                m.addConstr(gp.quicksum(
-                    x[model, node, c] for node in nodes for c in configs if (model, node, c) in x
-                ) >= 1)
+        #         m.addConstr(gp.quicksum(
+        #             x[model, node, c] for node in nodes for c in configs if (model, node, c) in x
+        #         ) >= 1)
 
-            for node in nodes:
-                m.addConstr(gp.quicksum(y[node, lid] for lid in range(len(valid_layouts))) == 1)
-                for c in configs:
-                    m.addConstr(
-                        gp.quicksum(
-                            x[model, node, c] for model in models if (model, node, c) in x
-                        ) <= gp.quicksum(
-                            y[node, lid] * layout.count(c)
-                            for lid, layout in enumerate(valid_layouts)
-                        )
-                    )
+        #     for node in nodes:
+        #         m.addConstr(gp.quicksum(y[node, lid] for lid in range(len(valid_layouts))) == 1)
+        #         for c in configs:
+        #             m.addConstr(
+        #                 gp.quicksum(
+        #                     x[model, node, c] for model in models if (model, node, c) in x
+        #                 ) <= gp.quicksum(
+        #                     y[node, lid] * layout.count(c)
+        #                     for lid, layout in enumerate(valid_layouts)
+        #                 )
+        #             )
 
-            m.setObjective(Z, GRB.MAXIMIZE)
-            m.setParam("OutputFlag", 0)
-            m.optimize()
+        #     m.setObjective(Z, GRB.MAXIMIZE)
+        #     m.setParam("OutputFlag", 0)
+        #     m.optimize()
 
-            throughput_vals = {model: T_m[model].X for model in models}
-            assignment = {
-                (model, node, c): int(x[model, node, c].X)
-                for model in models for node in nodes for c in configs
-                if (model, node, c) in x and x[model, node, c].X > 0.5
-            }
-            return throughput_vals, assignment
+        #     throughput_vals = {model: T_m[model].X for model in models}
+        #     assignment = {
+        #         (model, node, c): int(x[model, node, c].X)
+        #         for model in models for node in nodes for c in configs
+        #         if (model, node, c) in x and x[model, node, c].X > 0.5
+        #     }
+        #     return throughput_vals, assignment
 
-        # Leximin loop
-        locked = {}
-        for _ in range(len(models)):
-            T_vals, assignment = solve_leximin(locked)
-            unlocked = [m for m in models if m not in locked]
-            if not unlocked:
-                break
-            min_model = min(unlocked, key=lambda m: T_vals[m])
-            locked[min_model] = T_vals[min_model]
+        # # Leximin loop
+        # locked = {}
+        # for _ in range(len(models)):
+        #     T_vals, assignment = solve_leximin(locked)
+        #     unlocked = [m for m in models if m not in locked]
+        #     if not unlocked:
+        #         break
+        #     min_model = min(unlocked, key=lambda m: T_vals[m])
+        #     locked[min_model] = T_vals[min_model]
 
-        # static experiment alloc:
+        # static experiment alloc, ppl1:
         worker_configs = [
             (24, [all_models[0][1]]),
             (24, [all_models[0][1]]),
@@ -146,6 +156,18 @@ class Simulation(object):
             (6, [all_models[0][3]]),
             (6, [all_models[0][0], all_models[0][2]])
         ]
+
+        # static experiment alloc, ppl2:
+        # worker_configs = [
+        #     (12, [all_models[1][0]]),
+        #     (12, [all_models[1][1], all_models[1][2]]),
+        #     (12, [all_models[1][3]]),
+        #     (12, [all_models[1][3]]),
+        #     (12, [all_models[1][3]]),
+        #     (12, [all_models[1][3]]),
+        #     (12, [all_models[1][3]]),
+        #     (12, [all_models[1][3]])
+        # ]
 
         # static Gurobi alloc:
         # for (model_idxs, node, c), count in assignment.items():
