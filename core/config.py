a190c42b691e21bba17c803067119039516f4f50
Tami Takada <tamitakada@gmail.com>
2025-07-13 10:56:52 -0400

    template config

--- core/config.py
+++ core/config.py
@@ -1,19 +1,13 @@
 """ --------      Worker Machines Parameters      -------- """
 GPU_MEMORY_SIZE = 24000000  # in KB, 24GB for NVIDIA A30
-TOTAL_NUM_OF_NODES = 4
+TOTAL_NUM_OF_NODES = 8
 VALID_WORKER_SIZES = [24000000, 12000000, 6000000]
 
 """  --------       Workload Parameters    --------  """
-TOTAL_NUM_OF_JOBS_PER_WORKFLOW = {0: 10000, 1: 0}
+TOTAL_NUM_OF_JOBS_PER_WORKFLOW = {}
 
-SEND_RATES_BY_WORKFLOW = {
-    0: {"SEND_RATES": [55, 125, 95],
-        "SEND_RATE_CHANGE_INTERVALS": [1000, 3000],
-        "SEND_RATE_CHANGE_CURVES": []}, # TODO: STEP | LINEAR | EXPONENTIAL
-    1: {"SEND_RATES": [24, 6, 16],
-        "SEND_RATE_CHANGE_INTERVALS": [2000, 2000],
-        "SEND_RATE_CHANGE_CURVES": []},
-}
+# TODO: STEP | LINEAR | EXPONENTIAL
+SEND_RATES_BY_WORKFLOW = {}
 
 WORKLOAD_DISTRIBUTION = "POISON"  # UNIFORM | POISON | GAMMA
 
@@ -30,14 +24,15 @@ RESCHEDULE_THREASHOLD = 1.5
 """  -------        Shepherd Parameters  --------- """
 FLEX_LAMBDA = 3.03
 HERD_K = 1.3
+import numpy as np
 HERD_PERIODICITY = 10000   # runs HERD every [HERD_PERIODICITY] ms
 
 """  -------        General Scheduling Parameters  --------- """
-ENABLE_DYNAMIC_MODEL_LOADING = True
+ENABLE_DYNAMIC_MODEL_LOADING = False
 
 # HERD | VORTEX | CUSTOM
 # NOTE: HERD requires ENABLE_DYNAMIC_MODEL_LOADING
-ALLOCATION_STRATEGY = "HERD"
+ALLOCATION_STRATEGY = "CUSTOM"
 
 """
     If ALLOCATION_STRATEGY == "CUSTOM", must define CUSTOM_ALLOCATION : list[tuple[int, list[int]]]
@@ -53,8 +48,7 @@ ALLOCATION_STRATEGY = "HERD"
 
     Otherwise, workers will attempt to preload all models in the list for a static allocation.
 """
-CUSTOM_ALLOCATION = [(24, [1]), (24, [1]), (24, [1]), (6, [3]), (6, [3]), (6, [3]), (6, [0, 2]),
-                     (12, [4]), (12, [5,6]), (12, [7]), (12, [7]), (12, [7]), (12, [7]), (12, [7]), (12, [7])]
+CUSTOM_ALLOCATION = []
 
 # static experiment alloc, ppl1:
 # [(24, [1]), (24, [1]), (24, [1]), (6, [3]), (6, [3]), (6, [3]), (6, [0, 2])]
