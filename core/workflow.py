d11534067aefe20af47da7fe204ed03d9639a23a
Tami Takada <tamitakada@gmail.com>
2025-07-21 17:01:23 -0400

    emit sizes & ppl1 exec updates

--- core/workflow.py
+++ core/workflow.py
@@ -1,11 +1,11 @@
 """  --------       Workflow Parameters     --------  """
 # https://keras.io/api/applications/
 
-# TODO: mean instead of median; cv for pipeline 1
+import numpy as np
 WORKFLOW_LIST = [
     {"JOB_TYPE": 0,         # ID of the type of workflow (dependency graph)
      "JOB_NAME": "textvision",
-     "SLO": 303.5,
+     "SLO": np.inf, # 253.5,
      # the minimum amount of time necessary to execute the whole job
      "BEST_EXEC_TIME": 51.7,
      "TASKS": [
@@ -18,16 +18,27 @@ WORKFLOW_LIST = [
                 "INPUT_SIZE": 1,
                 "OUTPUT_SIZE": 2,            # in kB
                 "EXECUTION_TIME": 10,        # avg time, in ms
-                "MAX_BATCH_SIZE": 6,
+                "MAX_BATCH_SIZE": 4,
+                "MAX_EMIT_BATCH_SIZE": 4,
                 "MAX_WAIT_TIME": 1,         # ms
-                "BATCH_SIZES": [1, 2, 4, 6],
-                "BATCH_EXEC_TIME": [10, 10, 10, 11],
+                "BATCH_SIZES": [1, 2, 4],
+                "BATCH_EXEC_TIME": [8.1, 8.7, 9.0],
                 "MIG_BATCH_EXEC_TIMES": {
-                    24: [10, 10, 10, 11],
-                    12: [9, 9, 9, 10],
-                    6: [7, 7, 7, 8]
+                    24: [8.1, 8.7, 9.0],
+                    12: [8.1, 8.7, 9.0],
+                    6: [8.1, 8.7, 9.0],
                 },
-                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.05,
+                # "MIG_BATCH_EXEC_TIMES": {
+                #     24: [8.1, 8.7, 9.0],
+                #     12: [8.1, 8.7, 9.0],
+                #     6: [8.1, 8.7, 9.0],
+                # },
+                # "MIG_BATCH_EXEC_TIMES": {
+                #     24: [10, 10, 10],
+                #     12: [9, 9, 9],
+                #     6: [10, 10, 10]
+                # },
+                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.06,
                 "SLO": 50},
                {"MODEL_NAME": "vision_encoder",
                 "MODEL_ID": 1,
@@ -39,11 +50,12 @@ WORKFLOW_LIST = [
                 "OUTPUT_SIZE": 1000,
                 "EXECUTION_TIME": 31,        # in ms
                 "MAX_BATCH_SIZE": 16,
+                "MAX_EMIT_BATCH_SIZE": 4,
                 "MAX_WAIT_TIME": 10,        # ms
-                "BATCH_SIZES": [1, 4, 8, 16],
-                "BATCH_EXEC_TIME": [31, 98, 183, 349],
-                "MIG_BATCH_EXEC_TIMES": {24: [31, 98, 183, 349]},
-                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.05,
+                "BATCH_SIZES": [1, 2, 4, 8, 12, 16],
+                "BATCH_EXEC_TIME": [37.2, 67.3, 126.4, 237.7, 334.8, 454.5],
+                "MIG_BATCH_EXEC_TIMES": {24: [37.2, 67.3, 126.4, 237.7, 334.8, 454.5]}, # {24: [31, 98, 183, 349]},
+                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.02,
                 "SLO": 155},
                {"MODEL_NAME": "flmr",
                 "MODEL_ID": 2,
@@ -54,16 +66,22 @@ WORKFLOW_LIST = [
                 "INPUT_SIZE": 1002,
                 "OUTPUT_SIZE": 10,
                 "EXECUTION_TIME": 1.7,       # in ms
-                "MAX_BATCH_SIZE": 32,
+                "MAX_BATCH_SIZE": 8,
+                "MAX_EMIT_BATCH_SIZE": 16,
                 "MAX_WAIT_TIME": 1,         # ms
-                "BATCH_SIZES": [1, 2, 4, 8, 16, 32],
-                "BATCH_EXEC_TIME": [1.7, 1.9, 1.9, 2, 2.6, 3.1],
+                "BATCH_SIZES": [1, 2, 4, 8],
+                "BATCH_EXEC_TIME": [2.2, 2.8, 4.8, 8.6],
                 "MIG_BATCH_EXEC_TIMES": {
-                    24: [1.7, 1.9, 1.9, 2, 2.6, 3.1],
-                    12: [1.5, 1.7, 1.7, 1.8, 2.3, 2.8],
-                    6: [1.3, 1.4, 1.4, 1.5, 1.9, 2.3]
+                    24: [2.2, 2.8, 4.8, 8.6],
+                    12: [2.2, 2.8, 4.8, 8.6],
+                    6: [2.2, 2.8, 4.8, 8.6]
                 },
-                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.05,
+                # "MIG_BATCH_EXEC_TIMES": {
+                #     24: [1.3, 1.4, 1.4, 1.5], # [1.7, 1.9, 1.9, 2],
+                #     12: [1.5, 1.7, 1.7, 1.8],
+                #     6: [1.7, 1.9, 1.9, 2]
+                # },
+                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.95,
                 "SLO": 8.5},
                {"MODEL_NAME": "search",
                 "MODEL_ID": 3,
@@ -74,22 +92,26 @@ WORKFLOW_LIST = [
                 "INPUT_SIZE": 10,
                 "OUTPUT_SIZE": 10,
                 "EXECUTION_TIME": 18,        # in ms
-                "MAX_BATCH_SIZE": 32,
+                "MAX_BATCH_SIZE": 16,
+                "MAX_EMIT_BATCH_SIZE": 0, # NOTE: should not be used
                 "MAX_WAIT_TIME": 1,        # ms
-                "BATCH_SIZES": [1, 4, 8, 16, 32],
-                "BATCH_EXEC_TIME": [18, 64, 114, 209, 418],
+                "BATCH_SIZES": [1, 4, 8, 16],
+                "BATCH_EXEC_TIME": [14.6, 54.1, 108.5, 212.1],
                 "MIG_BATCH_EXEC_TIMES": {
-                    24: [18, 64, 114, 209, 418],
-                    12: [14, 50, 90, 164, 328],
-                    6: [14, 50, 90, 164, 328]
+                    24: [14.6, 54.1, 108.5, 212.1],
+                    12: [14.6, 54.1, 108.5, 212.1],
+                    6: [14.6, 54.1, 108.5, 212.1]
+                    # 24: [14, 50, 90, 164],
+                    # 12: [14, 50, 90, 164],
+                    # 6: [18, 64, 114, 209]
                 },
-                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.05,
+                "EXEC_TIME_COEFFICIENT_OF_VARIATION": 0.06,
                 "SLO": 90}
                ]
      },
      {"JOB_TYPE": 1,
      "JOB_NAME": "tts",
-     "SLO": 861.8,
+     "SLO": np.inf, # 861.8,
      # the minimum amount of time necessary to execute the whole job
      "BEST_EXEC_TIME": 101.4,
      "TASKS": [{"MODEL_NAME": "audio_det",
@@ -102,6 +124,7 @@ WORKFLOW_LIST = [
                 "OUTPUT_SIZE": 2,            # in kB
                 "EXECUTION_TIME": 65,        # avg time, in ms
                 "MAX_BATCH_SIZE": 8,
+                "MAX_EMIT_BATCH_SIZE": 10,
                 "MAX_WAIT_TIME": 1,          # ms
                 "BATCH_SIZES": [1, 2, 4, 8],
                 "BATCH_EXEC_TIME": [65.0, 68.0, 69.4, 72.1],
@@ -121,6 +144,7 @@ WORKFLOW_LIST = [
                 "OUTPUT_SIZE": 2,
                 "EXECUTION_TIME": 16.7,      # in ms
                 "MAX_BATCH_SIZE": 8,
+                "MAX_EMIT_BATCH_SIZE": 10,
                 "MAX_WAIT_TIME": 1,          # ms
                 "BATCH_SIZES": [1, 2, 4, 8],
                 "BATCH_EXEC_TIME": [16.7, 17.2, 17.5, 17.5],
@@ -141,6 +165,7 @@ WORKFLOW_LIST = [
                 "OUTPUT_SIZE": 2,
                 "EXECUTION_TIME": 3.36,        # in ms
                 "MAX_BATCH_SIZE": 2,
+                "MAX_EMIT_BATCH_SIZE": 10,
                 "MAX_WAIT_TIME": 1,          # ms
                 "BATCH_SIZES": [1, 2],
                 "BATCH_EXEC_TIME": [3.36, 3.72],
@@ -159,6 +184,7 @@ WORKFLOW_LIST = [
                 "OUTPUT_SIZE": 4,
                 "EXECUTION_TIME": 87.3,         # in ms
                 "MAX_BATCH_SIZE": 1,
+                "MAX_EMIT_BATCH_SIZE": 0,
                 "MAX_WAIT_TIME": 1,          # ms
                 "BATCH_SIZES": [1],
                 "BATCH_EXEC_TIME": [87.3, 164],
